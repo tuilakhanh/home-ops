2895124110d7c30576446f2116b45dd69269e738
tuilakhanh <bacdau852@gmail.com>
2025-04-07 15:39:39 +0700

    feat(nix): derper

--- nix/module/tailscale.nix
+++ nix/module/tailscale.nix
@@ -1,13 +1,30 @@
 {
+  config,
   pkgs,
+  lib,
   ...
 }:
 {
   services.tailscale = {
     enable = true;
     useRoutingFeatures = "both";
-    extraUpFlags = [
-      "--advertise-routes=192.168.1.0/24"
+    extraSetFlags = [
+      "--advertise-routes=192.168.1.0/24,192.168.1.96/27"
+      "--advertise-exit-node=true"
     ];
+    derper = {
+      enable = true;
+      domain = "derper.tuilakhanh.id.vn";
+      openFirewall = true;
+      verifyClients = true;
+    };
+  };
+  services.nginx.enable = lib.mkForce false;
+
+  services.caddy = {
+    enable = true;
+    virtualHosts."derper.tuilakhanh.id.vn".extraConfig = ''
+      reverse_proxy 127.0.0.1:${toString config.services.tailscale.derper.port}
+    '';
   };
 }
