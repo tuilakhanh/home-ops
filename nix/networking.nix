b98afd9e0ffce39fdcbf61b8e230aa21ae65a68d
tuilakhanh <bacdau852@gmail.com>
2026-08-06 19:21:41 +0700

    chore: nix network stuff

--- nix/networking.nix
+++ nix/networking.nix
@@ -10,11 +10,27 @@
       "1.1.1.1#one.one.one.one"
       "1.0.0.1#one.one.one.one"
     ];
-    networkmanager.enable = true;
+    networkmanager = {
+      enable = true;
+      dns = lib.mkForce "none";
+      ensureProfiles.profiles.lan-static = {
+        connection = {
+          id = "lan-static";
+          type = "ethernet";
+          autoconnect = "true";
+          autoconnect-priority = "100";
+        };
+        ethernet.mac-address = "00:E0:4C:68:00:3F";
+        ipv4 = {
+          address1 = "192.168.1.36/24,192.168.1.1";
+          method = "manual";
+        };
+        ipv6.method = "auto";
+      };
+    };
     nat.enable = false;
     firewall.enable = false;
     dhcpcd.extraConfig = "nohook resolv.conf";
-    networkmanager.dns = lib.mkForce "none";
   };
 
   services.resolved = {
